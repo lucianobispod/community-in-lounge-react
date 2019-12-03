59b5f5ace0440ad2755c1c2d28b16de1898c06aa
FIC <FIC@info.local>
2019-12-03 12:20:40 -0200

    Base do projeto com pastas e bibliotecas necessarias, criadas e instaladas

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+//mdb
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+
+//pages
+import Home from './pages/Home';
+
+//rotas
+import { BrowserRouter, Route, Router, Link } from "react-router-dom";
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Rotas = (
+    <Router>
+        <Route exact path="/" component={Home} />
+        {/* <Route path="/" component={Home} /> */}
+    </Router>
+)
+
+ReactDOM.render(Rotas, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
