d28fbf6ffed1df45711395e1a060c816e3ac3bd0
FIC <FIC@info.local>
2019-12-03 12:58:24 -0200

    arquivo css e estrutura do componente header criada

---
+++ src/components/HeaderDefault.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class HeaderDefault extends Component {
+    render() {
+        return (
+            <div>
+            </div>
+        );
+    }
+}
+export default HeaderDefault;
