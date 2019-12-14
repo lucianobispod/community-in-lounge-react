eee4b7482961baa85f85c91de822cc55d80b582b
Carllotta <carllottadias@gmail.com>
2019-12-14 15:25:34 -0200

    meu perfil implementado

--- src/pages/perfil/Perfil.js
+++ src/pages/perfil/Perfil.js
@@ -5,104 +5,157 @@ import axios from 'axios';
 
 
 class Perfil extends Component {
+constructor(props){
+    super(props);
+    this.state = {
+        usuario:{
+
+            comunidade:{
+                
+            }
+        }
+
+    }
+}
+
     render() {
         return (
-            <div>
-                <section className='container-geral'>
+            <div className='container-geral'>
 
-                    <div className='barra-lateral'>
-                        <div className="container-foto-perfil">
-                        <label class="label-file" for="file">
-                            <i class="material-icons">
-                                add_photo_alternate
-                            </i>
-                            Alterar foto</label>
-                        <input id="file" type="file" accept="image/*"/>
+                <div className='barra-lateral'>
+
+                    <div className="container-foto-perfil">
+
+                        <div>
+                            <img className='perfil-foto' />
                         </div>
+
+                        <form className='form-foto'>
+
+                            <label for="foto">
+                                Alterar foto
+                            </label>
+
+                            {/* <input type='file' name='foto' /> */}
+                            <input type="file" />
+                                
+                        </form>
+
                     </div>
 
-                    <div className='container_comunidade-usuario'>
+                </div>
 
-                        <section className='section-usuario'>
 
-                            <div className='dados-usuario'>
-                                <h2 classsName="titulo-perfil-dados">Dados Pessoais:</h2>
-                                <form class="form-dados-usuario">
-                                    <div className="container-perfil-nome">
-                                        <label className="perfil-nome label_dados">Nome:</label>
-                                        <div className="div-margin-input">
-                                            <input className="nome-input-user input_dados"></input>
+                <div className='container_comunidade-usuario'>
+
+                    <section className='section-usuario'>
+
+                        <div className='dados-usuario'>
+
+                            <h2 >Dados Pessoais</h2>
+
+
+                            <form class="form-dados-usuario">
+
+                                <div className="div-perfil-dados">
+                                    <label>Nome</label>
+                                    <input className="input_dados" type='text' />
+                                </div>
+
+                                <div className="div-perfil-dados">
+                                    <label>Email</label>
+                                    <input className="input_dados" />
+                                </div>
+
+                                <div className="div-perfil-dados">
+                                    <label>Telefone</label>
+                                    <input className="input_dados" />
+                                </div>
+
+                                <div className="div-perfil-dados">
+                                    <label>Senha</label>
+                                    <input className="input_dados" />
+                                </div>
+
+
+                                <div className="div-genero">
+
+                                    <label>Gênero</label>
+                                    <div className="div-input-radio">
+
+
+                                        <div className='div-radio'>
+                                            <label for='masculino'>Masculino</label>
+                                            <input type="radio" name="masculino" value="masculino" />
                                         </div>
-                                    </div>
 
-                                    <div className="container-perfil-email">
-                                        <label className="perfil-email label_dados">Email:</label>
-                                        <div className="div-margin-input">
-                                            <input className="email-input-user input_dados"></input>
-                                        </div>
-                                    </div>
-
-                                    <div className="container-perfil-telefone">
-                                        <label className="perfil-telefone label_dados">Telefone:</label>
-                                        <div className="div-margin-input">
-                                            <input className="telefone-input-user input_dados"></input>
-                                        </div>
-                                    </div>
-
-                                    <div className="container-perfil-genero">
-
-                                        <label className="perfil-nome label_dados_radio">Gênero:</label>
-                                        <div className="div-margin-input-radio">
-
-                                            <div className="genero-radio">
-                                                <input className="nome-input-user input_dados_radio" type="radio" name="genero" value="masculino" />Masculino
-                                    </div>
-
-                                            <div className="genero-radio">
-                                                <input className="nome-input-user input_dados_radio" type="radio" name="genero" value="feminino" />Feminino
-                                    </div>
-
+                                        <div className='div-radio'>
+                                            <label for='feminino'>Feminino</label>
+                                            <input type="radio" name="feminino" value="feminino" />
                                         </div>
 
-
                                     </div>
-                                </form>
-                                <div className="container-perfil-btn-alterar">
-                                    <button className="btn-editar-perfil">Editar</button>
+
+                                </div>
+
+                            </form>
+
+
+                            <button className="btn-editar-perfil">Editar</button>
+
+                        </div>
+
+
+                    </section>
+
+
+
+                    <hr className='perfil-linha'/>
+
+
+
+
+                    <section className='section-comunidade'>
+                        <div className='dados-comunidade'>
+
+                            <h2>Minha comunidade</h2>
+
+                            <div className="card-comunidade">
+
+                                <div className="foto-card-comunidade"><img src="" /></div>
+
+                                <div className="dados-card-comunidade">
+                                    <div className='div'>
+                                        <p>Nerdzão</p>
+                                    </div>
+
+                                    <div className='div font-pequena'>
+                                        <p>nerdzao@gmail.com</p>
+                                    </div>
+
+                                    <div className='div font-pequena'>
+                                        <p>119876543</p>
+                                    </div>
+
+                                    <div className="descricao-comunidade">
+                                        <p>O NerdZão é um grupo de estudos gratuito com sede em São Paulo. O foco principal do grupo é promover o networking e disseminar o conhecimento sobre softwares, plataformas, tecnologias e linguagens de programação de forma divertida.</p>
+                                    </div>
                                 </div>
 
                             </div>
 
+                            <button className="btn-editar-perfil">Editar</button>
 
-                        </section>
+                        </div>
+                    </section>
 
-                        <hr></hr>
 
-                        <section className='section-comunidade'>
-                            <div className='dados-comunidade'>
-                                <h2 className="titulo-perfil-comunidade">Dados da comunidade:</h2>
-                                <div className="card-comunidade">
-                                    <div className="foto-card-comunidade"><img src="" /></div>
-                                    <div className="dados-card-comunidade">
-                                        <p>Nome: Nerdzão</p>
-                                        <p>Email de contato: nerdzao@gmail.com</p>
-                                        <p>Telefone responsável: 119876543</p>
-                                        <div className="descricao-comunidade">
-                                        <p>Descrição: O NerdZão é um grupo de estudos gratuito com sede em São Paulo. O foco principal do grupo é promover o networking e disseminar o conhecimento sobre softwares, plataformas, tecnologias e linguagens de programação de forma divertida, quebrando o paradigma de complexidade no aprendizado.</p>
-                                        </div>
-                                    </div>
-                                    
-                                </div>
-                                <div className="container-comunidade-btn-alterar">
-                                    <button className="btn-editar-perfil">Editar</button>
-                                </div>
 
-                            </div>
-                        </section>
 
-                    </div>
+                </div>
 
-                </section>
+
+
             </div>
         );
     }
