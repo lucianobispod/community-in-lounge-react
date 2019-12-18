554510458a04d86224b171ed27031ea72c2ba62b
silinhasolivieira <54956902+silinhasolivieira@users.noreply.github.com>
2019-12-18 14:48:30 -0300

    pag atualizada

--- src/pages/cadastrar_usuario/CadastrarUsuario.js
+++ src/pages/cadastrar_usuario/CadastrarUsuario.js
@@ -9,20 +9,20 @@ class CadastrarUsuario extends Component {
     constructor(props) {
         super(props);
         this.state = {
-                nome: '',
-                email: '',
-                telefone: '',
-                senha: '',
-                genero: 'Masculino',
-                tipo_usuario_id: 3,
-                foto: React.createRef(),
-            
+            nome: '',
+            email: '',
+            telefone: '',
+            senha: '',
+            genero: 'Masculino',
+            tipo_usuario_id: 3,
+            foto: React.createRef(),
+
         }
     }
 
-    atualizaState = (event) =>{
+    atualizaState = (event) => {
         this.setState({
-        [event.target.name] : event.target.value
+            [event.target.name]: event.target.value
         });
     }
 
@@ -31,23 +31,23 @@ class CadastrarUsuario extends Component {
 
 
 
-    loginAutomatic= (email,senha) => {
-            
-        axios.post('http://localhost:5000/api/Login',
-       {
-           email: email,
-           senha: senha
-       }).then(res => {
-           if (res.status === 200) {
-               localStorage.setItem('usuario-token', res.data.token);
-                   this.props.history.push('/Meuseventos');
-           }
-       }).catch(erro => {
-           this.setState({ erroMessage: 'Email ou Senha invalido!' });
-           console.log(erro);
+    loginAutomatic = (email, senha) => {
 
-       });
-   }
+        axios.post('http://localhost:5000/api/Login',
+            {
+                email: email,
+                senha: senha
+            }).then(res => {
+                if (res.status === 200) {
+                    localStorage.setItem('usuario-token', res.data.token);
+                    this.props.history.push('/Meuseventos');
+                }
+            }).catch(erro => {
+                this.setState({ erroMessage: 'Email ou Senha invalido!' });
+                console.log(erro);
+
+            });
+    }
 
 
 
@@ -62,7 +62,7 @@ class CadastrarUsuario extends Component {
 
     efetuarCadastro = (event) => {
         event.preventDefault()
-        
+
         console.log(this.state.nome);
         console.log(this.state.senha);
         console.log(this.state.telefone);
@@ -82,43 +82,43 @@ class CadastrarUsuario extends Component {
             foto: 'url',
             senha: this.state.senha,
             genero: this.state.genero,
-            tipoUsuarioId:  this.state.tipo_usuario_id
+            tipoUsuarioId: this.state.tipo_usuario_id
         })
-        .then(resposta => {
-            console.log(resposta);
-            console.log(resposta.data.usuarioId);
-            if (resposta.status === 200) {
-                console.log('FOOOI');
-                this.uploadFoto(resposta.data.usuarioId);
+            .then(resposta => {
+                console.log(resposta);
+                console.log(resposta.data.usuarioId);
+                if (resposta.status === 200) {
+                    console.log('FOOOI');
+                    this.uploadFoto(resposta.data.usuarioId);
 
-                this.loginAutomatic(resposta.data.email,resposta.data.senha)
-            }else{
-                
-                window.location.href='/Login';
-            }
-        }).catch(error => {console.log(error)});
+                    this.loginAutomatic(resposta.data.email, resposta.data.senha)
+                } else {
+
+                    window.location.href = '/Login';
+                }
+            }).catch(error => { console.log(error) });
     }
 
-previwImage = () => {
+    previwImage = () => {
 
-    var input = document.getElementById('campo_foto_usuario').files[0];
+        var input = document.getElementById('campo_foto_usuario').files[0];
 
-    var imagem = document.getElementById('img');  
-    
-    const fileReader = new FileReader();
-    
-    fileReader.onloadend = () =>{
-        imagem.setAttribute('src', fileReader.result);
+        var imagem = document.getElementById('img');
+
+        const fileReader = new FileReader();
+
+        fileReader.onloadend = () => {
+            imagem.setAttribute('src', fileReader.result);
+        }
+
+        fileReader.readAsDataURL(input);
+
     }
-    
-    fileReader.readAsDataURL(input);
-
-}
 
 
     uploadFoto = (id) => {
         let evento = new FormData();
-        
+
         evento.set("imagem", this.state.foto.current.files[0]);
 
         axios({
@@ -137,7 +137,7 @@ previwImage = () => {
         return (
             <div>
                 <main>
-            
+
                     <section className="usuario_section">
 
                         <div className="text_container_usuario">
@@ -148,18 +148,19 @@ previwImage = () => {
                                 <h2>Cadastro de Usuario </h2>
                             </div>
 
-                            <div>
-                                <img id='img'/>
+                            <div id='img'>
+                                {/* <img id='img' /> */}
+                                <h4 className="img_h4"> Foto </h4>
                             </div>
 
                             <div className="campo_usuario_foto">
 
-                             
+
                                 <input type="file"
                                     id="campo_foto_usuario"
                                     onChange={this.previwImage}
                                     ref={this.state.foto}
-                                   />
+                                />
                             </div>
 
                             <div className="circle_usuario baixa_usuario"></div>
@@ -167,53 +168,58 @@ previwImage = () => {
 
                         <div className="usuario_container">
 
+                            <div className="home_x">
+                            </div>
+
                             <form className="usuario_form" onSubmit={this.efetuarCadastro}>
 
                                 <div className="inpu_div_usuario">
 
-        <div className="campo_usuario">
-                                    <label for="nome">Nome</label>
-        <input type="text" className="inpu-dado-usuario" name="nome" value={this.state.nome} onChange={this.atualizaState}/>
-        </div>
+                                    <div className="campo_usuario">
+                                        <label for="nome">Nome</label>
+                                        <input type="text" className="inpu-dado-usuario" name="nome" value={this.state.nome} onChange={this.atualizaState} />
+                                    </div>
 
 
-        <div className="campo_usuario">
+                                    <div className="campo_usuario">
                                         <label for="telefone">Telefone</label>
-        <input type="text" className="inpu-dado-usuario" name="telefone" value={this.telefone} onChange={this.atualizaState}/>
+                                        <input type="text" className="inpu-dado-usuario" name="telefone" value={this.telefone} onChange={this.atualizaState} />
 
-        </div>
-
-        <div className="genero">                 
-                                    <label>Gênero</label>
-        <div className="div-input-radio">
+                                    </div>
 
 
-        <div className='div-radio-genero'>
-                                            <label for=''>Masculino</label>
-        <input type="radio" name="genero" value="Masculino" checked={this.state.genero === 'Masculino'} onChange={this.atualizaState}/>
-        </div>
 
-        <div className='div-radio'>
-                                            <label for=''>Feminino</label>
-        <input type="radio" name="genero" value="Feminino" checked={this.state.genero === 'Feminino'} onChange={this.atualizaState}/>
-        </div>
-
-        </div>
-                                            
-        </div>
-
-        <div className="campo_usuario">
+                                    <div className="campo_usuario">
                                         <label for="email">Email</label>
-        <input type="email" className="inpu-dado-usuario" value={this.email} onChange={this.atualizaState} name="email"/>
-                                   
-         </div>
+                                        <input type="email" className="inpu-dado-usuario" value={this.email} onChange={this.atualizaState} name="email" />
 
-        <div className="campo_usuario">
+                                    </div>
+
+                                    <div className="campo_usuario">
                                         <label for="senha">Senha</label>
-        <input type="password" className="inpu-dado-usuario" name="senha" value={this.senha} onChange={this.atualizaState}/>
-        </div>
+                                        <input type="password" className="inpu-dado-usuario" name="senha" value={this.senha} onChange={this.atualizaState} />
+                                    </div>
 
-        </div>
+                                    <div className="genero">
+                                        <label>Gênero</label>
+                                        <div className="div-input-radio">
+
+
+                                            <div className='div-radio-genero'>
+                                                <label for=''>Masculino</label>
+                                                <input type="radio" name="genero" value="Masculino" checked={this.state.genero === 'Masculino'} onChange={this.atualizaState} />
+                                            </div>
+
+                                            <div className='div-radio'>
+                                                <label for=''>Feminino</label>
+                                                <input type="radio" name="genero" value="Feminino" checked={this.state.genero === 'Feminino'} onChange={this.atualizaState} />
+                                            </div>
+
+                                        </div>
+
+                                    </div>
+
+                                </div>
 
                                 <button className="button-cadastrar-usuario" type="submit">Cadastrar</button>
                             </form>
@@ -221,11 +227,11 @@ previwImage = () => {
 
                     </section>
 
-                <Footer />
                 </main>
-               
+
             </div>
         );
-    }}
+    }
+}
 
 export default CadastrarUsuario;
